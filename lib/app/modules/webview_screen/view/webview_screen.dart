2d4946c0504f6a5ff94060b5d4fccebc0ced2273
abir8 <abir80503@gmail.com>
2024-03-24 14:58:30 +0600

    testing webview

--- lib/app/modules/webview_screen/view/webview_screen.dart
+++ lib/app/modules/webview_screen/view/webview_screen.dart
@@ -52,125 +52,113 @@ class _WebviewScreenState extends State<WebviewScreen> {
       onPopInvoked: handleWillPop,
       child: SafeArea(
         child: Scaffold(
-          body: Stack(
-            children: [
-              InAppWebView(
-                key: webViewKey,
-                initialUrlRequest: URLRequest(url: WebUri(widget.loadUrl)),
-                initialOptions: InAppWebViewGroupOptions(
-                    crossPlatform: InAppWebViewOptions(
-                        javaScriptEnabled: true,
-                        useOnDownloadStart: true,
-                        useOnLoadResource: true,
-                        useShouldOverrideUrlLoading: true,
-                        cacheEnabled: true,
-                        preferredContentMode: UserPreferredContentMode.MOBILE,
-                        useShouldInterceptAjaxRequest: true,
-                        mediaPlaybackRequiresUserGesture: true,
-                        allowFileAccessFromFileURLs: true,
-                        allowUniversalAccessFromFileURLs: true),
-                    android: AndroidInAppWebViewOptions(
-                      useHybridComposition: true,
-                      allowFileAccess: true,
-                      allowContentAccess: true,
-                      geolocationEnabled: true,
-                      saveFormData: true,
-                    ),
-                    ios: IOSInAppWebViewOptions(
-                      allowsAirPlayForMediaPlayback: true,
-                      suppressesIncrementalRendering: true,
-                      ignoresViewportScaleLimits: true,
-                      selectionGranularity: IOSWKSelectionGranularity.DYNAMIC,
-                      isPagingEnabled: true,
-                      enableViewportScale: true,
-                      sharedCookiesEnabled: true,
-                      automaticallyAdjustsScrollIndicatorInsets: true,
-                      useOnNavigationResponse: true,
-                      allowsInlineMediaPlayback: true,
-                    )),
-                pullToRefreshController: pullToRefreshController,
-                onWebViewCreated: (controller) {
-                  webViewController = controller;
-                },
-                onCreateWindow:
-                    (inAppWebViewController, createWindowRequest) async {
-                  inAppWebViewController.addJavaScriptHandler(
-                      handlerName: 'openDRMOKWindow', callback: (args) {});
-                  return null;
-                },
-                onLoadStart: (controller, url) {
-                  setState(() {
-                    this.url = url.toString();
-                    urlController.text = this.url;
-                  });
-                },
-                androidOnPermissionRequest: (controller, origin,
-                        resources) async =>
-                    PermissionRequestResponse(
-                        resources: resources,
-                        action: PermissionRequestResponseAction.GRANT),
-                shouldOverrideUrlLoading: (controller, navigationAction) async {
-                  var uri = navigationAction.request.url!;
+          body: InAppWebView(
+            key: webViewKey,
+            initialUrlRequest: URLRequest(url: WebUri(widget.loadUrl)),
+            initialOptions: InAppWebViewGroupOptions(
+                crossPlatform: InAppWebViewOptions(
+                    javaScriptEnabled: true,
+                    useOnDownloadStart: true,
+                    useOnLoadResource: true,
+                    useShouldOverrideUrlLoading: true,
+                    cacheEnabled: true,
+                    preferredContentMode: UserPreferredContentMode.MOBILE,
+                    useShouldInterceptAjaxRequest: true,
+                    mediaPlaybackRequiresUserGesture: true,
+                    allowFileAccessFromFileURLs: true,
+                    allowUniversalAccessFromFileURLs: true),
+                android: AndroidInAppWebViewOptions(
+                  useHybridComposition: true,
+                  allowFileAccess: true,
+                  allowContentAccess: true,
+                  geolocationEnabled: true,
+                  saveFormData: true,
+                ),
+                ios: IOSInAppWebViewOptions(
+                  allowsAirPlayForMediaPlayback: true,
+                  suppressesIncrementalRendering: true,
+                  ignoresViewportScaleLimits: true,
+                  selectionGranularity: IOSWKSelectionGranularity.DYNAMIC,
+                  isPagingEnabled: true,
+                  enableViewportScale: true,
+                  sharedCookiesEnabled: true,
+                  automaticallyAdjustsScrollIndicatorInsets: true,
+                  useOnNavigationResponse: true,
+                  allowsInlineMediaPlayback: true,
+                )),
+            pullToRefreshController: pullToRefreshController,
+            onWebViewCreated: (controller) {
+              webViewController = controller;
+            },
+            onCreateWindow:
+                (inAppWebViewController, createWindowRequest) async {
+              inAppWebViewController.addJavaScriptHandler(
+                  handlerName: 'openDRMOKWindow', callback: (args) {});
+              return null;
+            },
+            onLoadStart: (controller, url) {
+              setState(() {
+                this.url = url.toString();
+                urlController.text = this.url;
+              });
+            },
+            androidOnPermissionRequest: (controller, origin,
+                    resources) async =>
+                PermissionRequestResponse(
+                    resources: resources,
+                    action: PermissionRequestResponseAction.GRANT),
+            shouldOverrideUrlLoading: (controller, navigationAction) async {
+              var uri = navigationAction.request.url!;
 
-                  if (![
-                    "http",
-                    "https",
-                    "file",
-                    "chrome",
-                    "data",
-                    "javascript",
-                    "about"
-                  ].contains(uri.scheme)) {
-                    if (!await canLaunch(url)) {
-                      // Launch the App
-                      await launch(
-                        url,
-                      );
-                      // and cancel the request
-                      return NavigationActionPolicy.CANCEL;
-                    }
-                  }
+              if (![
+                "http",
+                "https",
+                "file",
+                "chrome",
+                "data",
+                "javascript",
+                "about"
+              ].contains(uri.scheme)) {
+                if (!await canLaunch(url)) {
+                  // Launch the App
+                  await launch(
+                    url,
+                  );
+                  // and cancel the request
+                  return NavigationActionPolicy.CANCEL;
+                }
+              }
 
-                  return NavigationActionPolicy.ALLOW;
-                },
-                onLoadStop: (controller, url) async {
-                  pullToRefreshController.endRefreshing();
-                  setState(() {
-                    this.url = url.toString();
-                    urlController.text = this.url;
-                  });
-                },
-                onLoadError: (controller, url, code, message) {
-                  pullToRefreshController.endRefreshing();
-                },
-                onProgressChanged: (controller, progress) {
-                  if (progress == 100) {
-                    pullToRefreshController.endRefreshing();
-                  }
-                  setState(() {
-                    this.progress = progress / 100;
-                    urlController.text = url;
-                  });
-                },
-                onUpdateVisitedHistory: (controller, url, androidIsReload) {
-                  setState(() {
-                    this.url = url.toString();
-                    urlController.text = this.url;
-                  });
-                },
-                onConsoleMessage: (controller, consoleMessage) {
-                  print(consoleMessage.message);
-                },
-              ),
-              /*    Positioned(
-                bottom: 0,
-                right: 200,
-                left: 200,
-                child: progress < 1.0
-                    ? Center(child: CircularProgressIndicator(value: progress))
-                    : Container(),
-              )*/
-            ],
+              return NavigationActionPolicy.ALLOW;
+            },
+            onLoadStop: (controller, url) async {
+              pullToRefreshController.endRefreshing();
+              setState(() {
+                this.url = url.toString();
+                urlController.text = this.url;
+              });
+            },
+            onLoadError: (controller, url, code, message) {
+              pullToRefreshController.endRefreshing();
+            },
+            onProgressChanged: (controller, progress) {
+              if (progress == 100) {
+                pullToRefreshController.endRefreshing();
+              }
+              setState(() {
+                this.progress = progress / 100;
+                urlController.text = url;
+              });
+            },
+            onUpdateVisitedHistory: (controller, url, androidIsReload) {
+              setState(() {
+                this.url = url.toString();
+                urlController.text = this.url;
+              });
+            },
+            onConsoleMessage: (controller, consoleMessage) {
+              print(consoleMessage.message);
+            },
           ),
         ),
       ),
